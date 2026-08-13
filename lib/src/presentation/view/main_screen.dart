74446459437f9887bb4dc3335988deba346e00e6
Daylight <nytemod@gmail.com>
2026-08-13 14:21:10 +0200

    hooked up the sockets to the frontend ,and got the application working on desktop thankfully

--- lib/src/presentation/view/main_screen.dart
+++ lib/src/presentation/view/main_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:serafim/src/presentation/view/chat_list_page.dart';
+import 'package:serafim/src/presentation/view/chat_test_screen.dart';
 import 'package:serafim/src/presentation/viewmodel/navigation_notifier.dart';
 import 'package:serafim/src/utils/themes/app_colors.dart';
 
@@ -11,7 +12,10 @@ class MainScreen extends ConsumerWidget {
   // List of screens corresponding to each tab index
   static const List<Widget> _screens = [
     ChatListPage(),
-    Center(child: Text('Discover')),
+    ChatTestScreen(
+      recipientId: "4c3d4a54-ab64-4e54-945f-2886ae373319",
+      recipientName: "Hank",
+    ),
     Center(child: Text('Profile Screen')),
   ];
 
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,11 +6,9 @@ import FlutterMacOS
 import Foundation
 
 import flutter_secure_storage_macos
-import isar_flutter_libs
-import path_provider_foundation
+import isar_community_flutter_libs
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
   IsarFlutterLibsPlugin.register(with: registry.registrar(forPlugin: "IsarFlutterLibsPlugin"))
-  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
 }
